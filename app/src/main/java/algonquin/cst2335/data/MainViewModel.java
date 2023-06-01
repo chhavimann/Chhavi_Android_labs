1d5df2772d99afe614776b8e6d6f7ae2bdf81882
Chhavi Mann <mann0208@algonqinlive.com>
2023-06-01 13:04:21 -0400

    working on Lab 3

---
+++ app/src/main/java/algonquin/cst2335/data/MainViewModel.java
@@ -0,0 +1,20 @@
+package algonquin.cst2335.data;
+
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+
+public class MainViewModel extends ViewModel {
+
+    public String theText;
+    public String buttonText;
+    public MutableLiveData<String> editString =new MutableLiveData();
+    public MutableLiveData<Boolean> isSelected=new MutableLiveData<>();
+}
+
+
+
+
+
+
