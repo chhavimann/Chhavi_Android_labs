c05fc8f0841ce819b482080fc0e9c27f93bb01eb
Chhavi Mann <mann0208@algonqinlive.com>
2023-06-08 13:11:37 -0400

    Week3_TV

--- app/src/main/java/algonquin/cst2335/mann0208/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/mann0208/MainActivity.java
@@ -3,12 +3,48 @@ package algonquin.cst2335.mann0208;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        Log.d( TAG, "Message");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( "MainActivity", "The application is now visible" );
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( "MainActivity", "The application is now responding to user input" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( "MainActivity", "The application is now paused" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( "MainActivity", "The application is now stopped" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( "MainActivity", "The application is now destroyed" );
     }
 }
