646399f387c697efd2adff923186864d02e05335
Chhavi Mann <mann0208@algonqinlive.com>
2023-07-13 12:29:55 -0400

    Finished Test Cases and Java Docs

---
+++ app/src/main/java/algonquin/cst2335/mann0208/ChatRoom.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.mann0208;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatRoom extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+    }
+}
